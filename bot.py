59148061109b479ac2423290b0bf15cbda221c6c
lks2007 <lukas.soigneux@gmail.com>
2021-07-27 16:38:36 +0200

    create fonction convertTime() to minimize code

--- bot.py
+++ bot.py
@@ -23,97 +23,61 @@ class Client():
         self.disconnect = await disconnect(self.token)
 
 
+def convertTime(date):
+    date = datetime.strptime(date[:19], "%Y-%m-%d %H:%M:%S")
+    now = datetime.now()
+
+    year = now.year - date.year
+    month = now.month - date.month
+    day = now.day - date.day
+
+    hour = now.hour - date.hour
+    minute = now.minute - date.minute
+    second = now.second - date.second
+
+    if hour < 0:
+        day -= 1
+        hour = 24 + hour
+
+    if minute < 0:
+        hour -= 1
+        minute = 60 + minute
+
+    if second < 0:
+        minute -= 1
+        second = 60 + second
+
+    if year != 0:
+        r = str(year)+'Y '+str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
+        return r
+    elif month != 0:
+        r = str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
+        return r
+    elif day != 0:
+        r = str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
+        return r
+    elif hour != 0:
+        r = str(hour)+"h "+str(minute)+"min "+str(second)+"s"
+        return r
+    elif minute != 0:
+        r = str(minute)+"min "+str(second)+"s"
+        return r
+    elif second != 0:
+        r = str(second)+"s"
+        return r
+
 def getLog(*argv):
     if argv:
         with open('log/bot.log', 'r') as f:
             lines = f.readlines()
             last = lines[-1]
-            date = datetime.strptime(last[:19], "%Y-%m-%d %H:%M:%S")
-            now = datetime.now()
-    
-            year = now.year - date.year
-            month = now.month - date.month
-            day = now.day - date.day
-    
-            hour = now.hour - date.hour
-            minute = now.minute - date.minute
-            second = now.second - date.second
-    
-            if hour < 0:
-                day -= 1
-                hour = 24 + hour
-    
-            if minute < 0:
-                hour -= 1
-                minute = 60 + minute
-    
-            if second < 0:
-                minute -= 1
-                second = 60 + second
-    
-            if year != 0:
-                r = str(year)+'Y '+str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif month != 0:
-                r = str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif day != 0:
-                r = str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif hour != 0:
-                r = str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif minute != 0:
-                r = str(minute)+"min "+str(second)+"s"
-                return r
-            elif second != 0:
-                r = str(second)+"s"
-                return r
+            convertTime(last)
+            
     else:
         with open('log/daphne.log', 'r') as f:  
             lines = f.readlines()
             last = lines[-1]
-            date = datetime.strptime(last[:19], "%Y-%m-%d %H:%M:%S")
-            now = datetime.now()
-
-            year = now.year - date.year
-            month = now.month - date.month
-            day = now.day - date.day
-
-            hour = now.hour - date.hour
-            minute = now.minute - date.minute
-            second = now.second - date.second
-
-            if hour < 0:
-                day -= 1
-                hour = 24 + hour
-
-            if minute < 0:
-                hour -= 1
-                minute = 60 + minute
-
-            if second < 0:
-                minute -= 1
-                second = 60 + second
-
-            if year != 0:
-                r = str(year)+'Y '+str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif month != 0:
-                r = str(month)+"M "+str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif day != 0:
-                r = str(day)+"d "+str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif hour != 0:
-                r = str(hour)+"h "+str(minute)+"min "+str(second)+"s"
-                return r
-            elif minute != 0:
-                r = str(minute)+"min "+str(second)+"s"
-                return r
-            elif second != 0:
-                r = str(second)+"s"
-                return r
+            convertTime(last)
 
 if __name__ == "__main__":
     client = Client()
